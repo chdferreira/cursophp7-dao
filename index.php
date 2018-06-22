f59fc0307f69fe65340547bda5eb0cfeff0601fd
Carlos Ferreira <chdferreira@gmail.com>
2018-06-22 20:07:30 -0300

    Inserido metodos List, Login na classe Usuario

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
 	require_once("config.php");
 
-	$usuario = new Usuario();
+	//$usuario = new Usuario();
+	//$usuario->loadById(1);
+	//	echo $usuario;
 
-	$usuario->loadById(1);
+	//Carrega uma Lista de Usuários
+
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
+
+	//Carrega uma lista de usuários buscando pelo login
+
+	//$search = Usuario::search("te");
+	//echo json_encode($search);
+
+	//Carrega um usuário usando o login e a senha
+	$usuario = new Usuario();
+	$usuario->login("carlos","123456");
 
 	echo $usuario;
+
+
+
 
  ?>
